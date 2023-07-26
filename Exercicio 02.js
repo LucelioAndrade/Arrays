4ca93e00d6c78d845c5349a28f64df5f45aa343a
Lucélio Andrade <lucelio.andrade@gmail.com>
2023-07-26 19:24:33 -0300

    Arrays e loops

---
+++ Exercicio 02.js
@@ -0,0 +1,7 @@
+const numeros = [23, 12, 45, 5, 94, 7, 82];
+
+let i = 0;
+while (i < numeros.length) {
+    console.log (numeros [i]);
+    i++;
+}
---
+++ Exercicio 03.js
@@ -0,0 +1,8 @@
+const numeros = [23, 12, 45, 5, 94, 7, 82];
+let soma = 0
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
+
+console.log (soma);
